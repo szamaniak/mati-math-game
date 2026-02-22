4e6a00ad516133bfee24520420906c66351b01ad
szamaniak <a.m.nowiccy@gmail.com>
2026-02-22 21:12:28 +0100

    poprawki

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,25 +2,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBlgTR-2Ztd6-Nr_rQFGvWVYJkDBuyxlZU",
-  authDomain: "mati-math-game.firebaseapp.com",
-  projectId: "mati-math-game",
-  storageBucket: "mati-math-game.firebasestorage.app",
-  messagingSenderId: "622286563254",
-  appId: "1:622286563254:web:cfd6eba1fe4394af470317",
-  measurementId: "G-Q1N8GH0X7S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Musisz mieć 'export' przed każdą stałą, której chcesz użyć w AuthManager
 export const auth = getAuth(app);
